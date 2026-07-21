8659426dfdeecdfce89650ae7930b4757666bc84
Eloghene <eloghene@bananacrystal.com>
2026-07-21 18:34:21 +0100

    Update admin email in seed data from mwbc.com.ng to mbwc.org.ng

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.find_or_create_by!(email: "admin@mwbc.com.ng") do |user|
+User.find_or_create_by!(email: "admin@mbwc.org.ng") do |user|
   user.name = "Admin"
   user.password = "password123"
   user.password_confirmation = "password123"
